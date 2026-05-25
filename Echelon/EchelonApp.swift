1d98ac70a4915e21791ecaa5f039a9e2c1b143dc
Puzzite <jackson.jordan@xylasoft.com>
2026-05-25 17:46:10 -0500

    Initial Commit

---
+++ Echelon/EchelonApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchelonApp.swift
+//  Echelon
+//
+//  Created by Jackson Jordan on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct EchelonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EchelonTests/EchelonTests.swift
@@ -0,0 +1,17 @@
+//
+//  EchelonTests.swift
+//  EchelonTests
+//
+//  Created by Jackson Jordan on 5/25/26.
+//
+
+import Testing
+@testable import Echelon
+
+struct EchelonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
